9e65ad5791e141de72079e58af20eaafede84f05
vanessa-cl <vanessalima951@gmail.com>
2025-02-09 19:28:36 -0300

    criação de métodos para manipulação de fila de espera

---
+++ src/main/java/utils/FilaEsperaUtil.java
@@ -0,0 +1,82 @@
+package main.java.utils;
+
+import main.java.entities.Ouvinte;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
+public class FilaEsperaUtil {
+    private int idPalestra;
+    private ArrayDeque<Ouvinte> filaEspera = new ArrayDeque<>();
+    FileUtil fileUtil = new FileUtil();
+    String arquivo = "src/main/java/database/filaEspera.txt";
+
+    public FilaEsperaUtil(int idPalestra) {
+        lerOuvintesFilaEspera(idPalestra);
+        this.idPalestra = idPalestra;
+    }
+
+    public void lerOuvintesFilaEspera(int idPalestra) {
+        ArrayList<String> ouvintesDB = fileUtil.lerArquivo(arquivo);
+        for (String dado : ouvintesDB) {
+            String[] dadosOuvinte = dado.split(" - ");
+            if (Integer.parseInt(dadosOuvinte[1].split(": ")[1]) == idPalestra) {
+                Ouvinte novoOuvinte = new Ouvinte(
+                        Integer.parseInt(dadosOuvinte[0].split(": ")[1]),
+                        Integer.parseInt(dadosOuvinte[1].split(": ")[1])
+                );
+                filaEspera.add(novoOuvinte);
+            }
+        }
+    }
+
+    public boolean adicionarOuvinte(Ouvinte ouvinte) {
+        boolean resultado = false;
+        filaEspera.add(ouvinte);
+        try {
+            resultado = fileUtil.escreverArquivo(arquivo, ouvinte);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resultado;
+    }
+
+    public Ouvinte removerOuvinteInicio() {
+        Ouvinte ouvinteEncontrado = filaEspera.poll();
+        if (ouvinteEncontrado == null) {
+            return null;
+        }
+        try {
+            fileUtil.removerLinhaArquivoPeloId(arquivo, ouvinteEncontrado.getId(), idPalestra);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ouvinteEncontrado;
+    }
+
+    public void listarOuvintes() {
+        System.out.println("Fila de espera:");
+        for (Ouvinte ouvinte : filaEspera) {
+            System.out.println(ouvinte.toString());
+        }
+        System.out.println("____________________________________________________________________________________________________________________");
+    }
+
+    public int getQuantidadeOuvintes() {
+        return filaEspera.size();
+    }
+
+    public Ouvinte buscarOuvintePeloId(int idUsuario) {
+        Ouvinte resultado = null;
+        for (Ouvinte ouvinte : filaEspera) {
+            if (ouvinte.getId() == idUsuario) {
+                resultado = ouvinte;
+            }
+        }
+        return resultado;
+    }
+
+    public ArrayDeque<Ouvinte> getOuvintes() {
+        return filaEspera;
+    }
+}
